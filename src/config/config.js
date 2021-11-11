f5b19b53137d165e3ad2ab3281d93538080bf023
Mikiyas721 <Mikiyas721@gmail.com>
2021-11-11 15:26:15 -0600

    Revoked Token

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const configs = {
-    TOKEN: "1000620512:AAF8g7HtlbcdGB7SrfRVTcUFmHF-F-8CysI",
+    TOKEN: process.env.BOT_TOKEN,
     API_BASE_URL: "https://api.stackexchange.com/2.2",
     SERVER_URL:'https://stackoverflowrealbot.herokuapp.com',
     PRODUCTION_MODE: true,
-    stackKey: "X7hmoKyFazswM65No1lnsA((",
-    stackClientId: "17827",
-    stackClientSecret: "cRGRyBLLNg8zDknij7lq9w((",
+    stackKey: process.env.STACK_KEY,
+    stackClientId: process.env.STACK_CLIENT_ID,
+    stackClientSecret: process.env.STACK_CLIENT_SECRET,
 };
 
 
